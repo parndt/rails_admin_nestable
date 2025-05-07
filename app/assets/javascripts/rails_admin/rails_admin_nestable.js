d599f4c2bfbd37055479dea6281ca6b1e806b912
Philip Arndt <git@p.arndt.io>
2025-05-07 12:11:19 +1200

    Try sending csrf_token

--- app/assets/javascripts/rails_admin/rails_admin_nestable.js
+++ app/assets/javascripts/rails_admin/rails_admin_nestable.js
@@ -10,39 +10,57 @@
 
     updateNodes = function (treeNodes) {
       var serializedTree;
-      serializedTree = treeNodes.nestable('serialize');
+      serializedTree = treeNodes.nestable("serialize");
       return $.ajax({
-        url: treeNodes.data('update-path'),
-        type: 'POST',
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader("X-CSRF-Token", $("meta[name='csrf-token']").attr("content"))
+        },
+        url: treeNodes.data("update-path"),
+        type: "POST",
         data: {
           treeNodes: serializedTree
         },
         success: function (data) {
           var $flash;
-          $flash = $('<div>').addClass('nestable-flash alert alert-success').append($('<button>').addClass('close').data('dismiss', 'alert').html('&times;')).append($('<span>').addClass('body').html(data));
-          $('#rails_admin_nestable').append($flash);
+          $flash = $("<div>")
+            .addClass("nestable-flash alert alert-success")
+            .append(
+              $("<button>")
+                .addClass("close")
+                .data("dismiss", "alert")
+                .html("&times;")
+            )
+            .append(
+              $("<span>").addClass("body").html(data)
+            );
+          $("#rails_admin_nestable").append($flash);
           return $flash.fadeIn(200).delay(2000).fadeOut(200, function () {
             return $(this).remove();
           });
         }
       });
     };
-    $treeNodes = $('#tree_nodes');
+    $treeNodes = $("#tree_nodes");
     $treeNodesOptions = {};
-    $treeNodesMaxDepth = $treeNodes.data('max-depth');
-    $liveUpdate = $('#rails_admin_nestable input[type=checkbox]');
-    $updateButton = $('#rails_admin_nestable button');
-    liveUpdateMode = !$liveUpdate.length && !$updateButton.length ? true : $liveUpdate.prop('checked');
-    $('#rails_admin_nestable button').prop('disabled', $liveUpdate.prop('checked'));
+    $treeNodesMaxDepth = $treeNodes.data("max-depth");
+    $liveUpdate = $("#rails_admin_nestable input[type=checkbox]");
+    $updateButton = $("#rails_admin_nestable button");
+    if (!$liveUpdate.length && !$updateButton.length) {
+      liveUpdateMode = true;
+    }
+    else {
+      liveUpdateMode = $liveUpdate.prop("checked");
+    }
+    $("#rails_admin_nestable button").prop("disabled", $liveUpdate.prop("checked"));
     $liveUpdate.change(function () {
-      liveUpdateMode = $(this).prop('checked');
-      return $updateButton.prop('disabled', liveUpdateMode);
+      liveUpdateMode = $(this).prop("checked");
+      return $updateButton.prop("disabled", liveUpdateMode);
     });
     $updateButton.click(function () {
       return updateNodes($treeNodes);
     });
-    if ($treeNodesMaxDepth && $treeNodesMaxDepth !== 'false') {
-      $treeNodesOptions['maxDepth'] = $treeNodesMaxDepth;
+    if ($treeNodesMaxDepth && $treeNodesMaxDepth !== "false") {
+      $treeNodesOptions["maxDepth"] = $treeNodesMaxDepth;
     }
     return $treeNodes.nestable($treeNodesOptions).on({
       change: function (event) {
